13a3398fdea7583f84591d739a7761038a419b93
lucas leite <me@lucasleite.us>
2020-06-30 13:07:22 -0400

    Update env vars

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DEBUG = False
-SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", 'mysql+pymysql://root:root@localhost:3306/blackbox')
+DEBUG = os.environ.get("FLASK_DEBUG", False) == "True"
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", 'mysql+pymysql://root:blackbox@localhost:3306/blackbox')
 DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'blackbox'
--- run.py
+++ run.py
@@ -2,11 +2,11 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from app import app
-from app.jobs.start_serial_com import start_serial_com
+from app.jobs.start_arduino import start_arduino
 
 if __name__ == "__main__":
     scheduler = BackgroundScheduler(
         jobstores={'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')})
     scheduler.start()
-    job = scheduler.add_job(start_serial_com, id="start_serial_com", replace_existing=True)
-    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    job = scheduler.add_job(start_arduino, id="start_serial_com", replace_existing=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
